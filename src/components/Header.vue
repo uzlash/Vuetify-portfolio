f891cb32f5c46ea2b000bf9e97a0a12eec0c0539
Usman <Murtala>
2020-06-15 15:47:07 +0100

    Initialized project, added the header and home components

---
+++ src/components/Header.vue
@@ -0,0 +1,65 @@
+<template>
+  <nav>
+    <v-navigation-drawer
+      app
+      class="grey darken-4"
+      v-model="drawer"
+      width="350px"
+    >
+      <v-layout column align-center>
+        <v-flex class="mt-5">
+          <p
+            class="white--text mt-3 text-center text-h1 font-weight-bold orange--text"
+          >
+            UM
+          </p>
+        </v-flex>
+        <v-flex class="my-3 text-h4 font-weight-thin">
+          Usman Murtala
+        </v-flex>
+        <v-flex class="my-3 text-h5 font-weight-thin">
+          Front End Developer
+        </v-flex>
+      </v-layout>
+      <v-list>
+        <v-list-item-group class="my-5">
+          <v-list-item v-for="(item, i) in navItems" :key="i">
+            <v-list-item-content>
+              <v-list-item-title
+                class="white--text text-center text-h6 font-weight-thin"
+              >
+                {{ item }}
+              </v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+      <v-row wrap>
+        <v-col class="mt-5 text-center" md4 v-for="(icon, i) in icons" :key="i">
+          <v-icon>{{ icon }}</v-icon>
+        </v-col>
+      </v-row>
+    </v-navigation-drawer>
+  </nav>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      navItems: [
+        "About Me",
+        "Services",
+        "Experience",
+        "Skills and Education",
+        "Portfolio",
+        "Clients",
+        "Contact"
+      ],
+      icons: ["mdi-twitter", "mdi-linkedin", "mdi-github"]
+    };
+  }
+};
+</script>
+
+<style></style>
---
+++ src/components/Home.vue
@@ -0,0 +1,39 @@
+<template>
+  <section class="home">
+    <div class="home--elements">
+      <h2 class="text-h2 font-weight-thin">USMAN MURTALA</h2>
+      <h1 class="text-h1 font-weight-thin orange--text">FRONT END DEVELOPER</h1>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  }
+};
+</script>
+
+<script>
+export default {
+  name: "Home",
+
+  data: () => ({})
+};
+</script>
+
+<style>
+.home {
+  height: 100vh;
+  background: url("../assets/hero.png");
+  background-size: cover;
+}
+
+.home--elements {
+  position: absolute;
+  right: 5px;
+  bottom: 5px;
+  text-align: right;
+}
+</style>
